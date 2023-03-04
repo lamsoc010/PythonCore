bc3e17f1d566777a6aec6bc5569077f7d77dd76c
Trần Vĩnh Lãm <tranvinhlam01@gmail.com>
2023-03-04 13:10:08 +0700

    baitap2

--- BaiTap1-14Cau/BT4.py
+++ BaiTap1-14Cau/BT4.py
@@ -1,4 +1,5 @@
 # Bài 4: Sai rồi
+
 eps = 0.00000000000001 #Sai số
 x = float(input("Nhập x: "))
 i = 1
@@ -20,3 +21,7 @@ while(abs(second - first) > eps):
     else:
         second = first + ((phanTu / phanMau) * x**i)
 print("Giá trị là: ", first)
+step = 0
+step += 1
+
+1 * (i + step)
